095fe68f77734f318982f98ab3b28b2b65d67be6
yan <yaneliyahu83@gmail.com>
2023-10-10 20:37:59 +0300

    Add hover effects to image, title and creator

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -3,11 +3,21 @@ import Equilibrium from "../../assets/images/image-equilibrium.jpg";
 import NFTDetails from "../NFTDetails/NFTDetails";
 import HorizontalLine from "../UI/HorizontalLine/HorizontalLine";
 import NFTCreator from "../NFTCreator/NFTCreator";
+import { ReactComponent as ViewIcon } from "../../assets/svgs/icon-view.svg";
+import { useState } from "react";
 
 export default function NFTCard() {
+  const [isImgHover, setIsImgHover] = useState<boolean>(false);
+
   return (
     <div className="nft">
-      <img src={Equilibrium} alt="equilibrium" />
+      <div
+        onMouseEnter={() => setIsImgHover(true)}
+        onMouseLeave={() => setIsImgHover(false)}
+        className="nft__image"
+      >
+        {isImgHover && <ViewIcon className="nft__view-icon" />}
+      </div>
       <h1 className="nft__title">Equilibrium #3429</h1>
       <p className="nft__description">
         Our Equilibrium collection promotes balance and calm
