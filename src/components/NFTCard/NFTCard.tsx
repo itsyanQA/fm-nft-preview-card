3f6d6d60df78bf2d3ddee689ed6a8bc421b40f2a
yan <yaneliyahu83@gmail.com>
2023-10-10 21:08:58 +0300

    Add CodedBy.tsx component

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -5,26 +5,30 @@ import HorizontalLine from "../UI/HorizontalLine/HorizontalLine";
 import NFTCreator from "../NFTCreator/NFTCreator";
 import { ReactComponent as ViewIcon } from "../../assets/svgs/icon-view.svg";
 import { useState } from "react";
+import CodedBy from "../CodedBy/CodedBy";
 
 export default function NFTCard() {
   const [isImgHover, setIsImgHover] = useState<boolean>(false);
 
   return (
-    <div className="nft">
-      <div
-        onMouseEnter={() => setIsImgHover(true)}
-        onMouseLeave={() => setIsImgHover(false)}
-        className="nft__image"
-      >
-        {isImgHover && <ViewIcon className="nft__view-icon" />}
+    <>
+      <div className="nft">
+        <div
+          onMouseEnter={() => setIsImgHover(true)}
+          onMouseLeave={() => setIsImgHover(false)}
+          className="nft__image"
+        >
+          {isImgHover && <ViewIcon className="nft__view-icon" />}
+        </div>
+        <h1 className="nft__title">Equilibrium #3429</h1>
+        <p className="nft__description">
+          Our Equilibrium collection promotes balance and calm
+        </p>
+        <NFTDetails />
+        <HorizontalLine />
+        <NFTCreator />
       </div>
-      <h1 className="nft__title">Equilibrium #3429</h1>
-      <p className="nft__description">
-        Our Equilibrium collection promotes balance and calm
-      </p>
-      <NFTDetails />
-      <HorizontalLine />
-      <NFTCreator />
-    </div>
+      <CodedBy />
+    </>
   );
 }
